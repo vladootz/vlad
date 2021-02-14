eace1807c610970bd065f4cecf83f010a1936d09
Vlad Nastasiu <vlad@nastasiu.com>
2021-02-14 02:06:29 +0200

    Fix firefox mobile, update npm

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,9 @@
 module.exports = {
   title: 'all takes time ← and',
   description: '',
+  head: [
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
+  ],
   themeConfig: {
     search: false,
     nav: [
